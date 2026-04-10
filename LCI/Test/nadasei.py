c52c7bfd0097b8f5167ce3f48b022d4879041cac
Felipe-ps258 <felipe.ps258@gmail.com>
2026-04-10 11:15:30 -0300

    Add files via upload

---
+++ LCI/Test/nadasei.py
@@ -0,0 +1,14 @@
+a = int(input('Digite sua nota: '))
+b = int(input('Digite sua nota: '))
+c = int(input('Digite sua nota: '))
+A = int(input('Digite a nota: '))
+B = int(input('Digite a nota: '))  
+C = int(input('Digite a nota: '))
+
+media1 = (a + b + c)/ 3
+media2 = (A + B + C)/ 3
+
+if media1 > media2:
+    print('Você é melhor que o outro')
+else:
+    print('Tu é muito burro')
--- LCI/Test/test.py
+++ LCI/Test/test.py
@@ -1,3 +1,10 @@
-dias = ("segunda", "terça", "quarta", "quinta", "sexta")
+nota1 = int(input("digite a 1° nota: "))
+nota2 = int(input("digite a 2° nota: "))
 
-int(input(""))
+if nota1 > nota2:
+    print('A primeira nota é maior que a segunda nota')
+
+else:
+    print('A segunda nota é maior que a primeira nota')
+
+print('Fim!')
